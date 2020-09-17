3a9bea4d3a39ff8531f34206a7004f01fe47f82b
Nathan Stratford <nstratford76@gmail.com>
2020-09-17 14:16:03 -0600

    Added code for meter to feet conversion

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp
 
             string location = "Rexburg, ID";
 
-            Console.WriteLine("Please enter your location: ");
+            Console.Write("Please enter your location: ");
             location = Console.ReadLine();
 
             Console.WriteLine($"Your name is  {name}");
@@ -24,6 +24,28 @@ namespace ConsoleApp
 
             var currentDate = DateTime.Now;
             Console.WriteLine("The current date is: " + currentDate.ToShortDateString());
+
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("Please enter width(in meters): ");
+            widthString = Console.ReadLine();
+
+            width = double.Parse(widthString);
+
+            Console.Write("Please enter height(in meters: ");
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
